81a62d60aab05c6fda9c2a7c37fe412b1ed61602
Gleb <gleb.prischepa@banuba.com>
2020-12-25 17:47:37 +0300

    Setup ve sdk integration for react-native Android

---
+++ android/app/src/main/java/com/vesdkreactnativeintegrationsample/videoeditor/impl/IntegrationTimerStateProvider.kt
@@ -0,0 +1,18 @@
+package com.vesdkreactnativeintegrationsample.videoeditor.impl
+import com.vesdkreactnativeintegrationsample.R
+import com.banuba.sdk.cameraui.data.CameraTimerStateProvider
+import com.banuba.sdk.cameraui.data.TimerEntry
+
+internal class IntegrationTimerStateProvider : CameraTimerStateProvider {
+
+    override val timerStates = listOf(
+            TimerEntry(
+                    durationMs = 0,
+                    iconResId = R.drawable.ic_stopwatch_off
+            ),
+            TimerEntry(
+                    durationMs = 3000,
+                    iconResId = R.drawable.ic_stopwatch_on
+            )
+    )
+}
