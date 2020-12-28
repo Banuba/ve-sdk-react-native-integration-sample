64ddcc6bd56db6570d71d375063b46e4272dbdda
Andrei Sak <andrei.sak@banuba.com>
2020-12-28 19:26:47 +0300

    Setup ve sdk integration for react-native iOS

---
+++ ios/VideoEditorModuleBridge.m
@@ -0,0 +1,14 @@
+//
+//  VideoEditorModuleBridge.m
+//  vesdkreactnativeintegrationsample
+//
+//  Created by Andrei Sak on 28.12.20.
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(VideoEditorModule, NSObject)
+
+RCT_EXTERN_METHOD(openVideoEditor)
+
+@end
