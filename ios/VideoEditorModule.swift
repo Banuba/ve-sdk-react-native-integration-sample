7359ec5f0efb54d9f8ef60e0d007842c14bf27ed
ve-sdk-ios <73183216+ve-sdk-ios@users.noreply.github.com>
2020-12-29 18:35:31 +0300

    Setup ve sdk integration for react-native iOS (#1)
    * Setup ve sdk integration for react-native iOS
    
    * add luts and bundleEffects files
    
    * Update description
    
    Co-authored-by: Andrei Sak <andrei.sak@banuba.com>
    Co-authored-by: Gleb Markin <gleb.markin@banuba.com>
    Co-authored-by: Gleb <gleb.prischepa@banuba.com>
    Co-authored-by: Gleb Prischepa <gleb.prischepa@expcapital.com>

---
+++ ios/VideoEditorModule.swift
@@ -0,0 +1,43 @@
+//
+//  VideoEditorModule.swift
+//  vesdkreactnativeintegrationsample
+//
+//  Created by Andrei Sak on 28.12.20.
+//
+
+import React
+import BanubaVideoEditorSDK
+import BanubaMusicEditorSDK
+import BanubaOverlayEditorSDK
+
+@objc(VideoEditorModule)
+class VideoEditorModule: NSObject, RCTBridgeModule {
+
+  @objc func openVideoEditor() {
+    guard let presentedVC = RCTPresentedViewController() else {
+      return
+    }
+    
+    let config = createVideoEditorConfiguration()
+    let videoEditor = BanubaVideoEditor(
+      token: "place AR token here",
+      effectsToken: "JKUsDuoT+mERLng/LVBT/SxK7fK+1u0DuoAruXXgIJhuSI0aynki+8gGXUWAC1H3jBDYThexyDBxlvZFZ7/2nzslMbIi26y2xh4P7GI=",
+      configuration: config,
+      externalViewControllerFactory: nil
+    )
+    DispatchQueue.main.async {
+      videoEditor.presentVideoEditor(from: presentedVC, animated: true, completion: nil)
+    }
+  }
+  
+  private func createVideoEditorConfiguration() -> VideoEditorConfig {
+    let config = VideoEditorConfig()
+    // Do customization here
+    return config
+  }
+  
+  // MARK: - RCTBridgeModule
+  static func moduleName() -> String! {
+    return "VideoEditorModule"
+  }
+}
