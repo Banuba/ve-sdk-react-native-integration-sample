fdae81394cef8858aa960de2477c6146b979fe60
Alexander <alexander.bernat@banuba.com>
2021-10-08 14:58:39 +0300

    migrate android to 1.19.0

--- android/app/src/main/java/com/vesdkreactnativeintegrationsample/ActivityStarterModule.kt
+++ android/app/src/main/java/com/vesdkreactnativeintegrationsample/ActivityStarterModule.kt
@@ -1,23 +1,26 @@
 package com.vesdkreactnativeintegrationsample
 
+import android.net.Uri
 import com.banuba.sdk.ve.flow.VideoCreationActivity
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 
-class ActivityStarterModule(reactContext: ReactApplicationContext)
-    : ReactContextBaseJavaModule(reactContext) {
+class ActivityStarterModule(reactContext: ReactApplicationContext) :
+    ReactContextBaseJavaModule(reactContext) {
 
     override fun getName(): String = "ActivityStarter"
 
     @ReactMethod
     fun navigateToVideoEditor() {
-        val intent = VideoCreationActivity.buildIntent(
-                reactApplicationContext,
-                // setup data that will be acceptable during export flow
-                null,
-                // set TrackData object if you open VideoCreationActivity with preselected music track
-                null
+        val intent = VideoCreationActivity.startFromCamera(
+            reactApplicationContext,
+            //setup video uri to utilize it with Picture in Picture mode
+            Uri.EMPTY,
+            // setup data that will be acceptable during export flow
+            null,
+            // set TrackData object if you open VideoCreationActivity with preselected music track
+            null
         )
         reactApplicationContext.startActivityForResult(intent, 1, null);
     }
