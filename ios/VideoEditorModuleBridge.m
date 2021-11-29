111ab4ef983b82d611bf1ea0af7bc2347831e7df
Gleb Markin <gleb.markin@banuba.com>
2021-11-29 16:02:50 +0300

    Apply RCTPromiseResolveBlock on App.js iOS

--- ios/VideoEditorModuleBridge.m
+++ ios/VideoEditorModuleBridge.m
@@ -9,6 +9,6 @@
 
 @interface RCT_EXTERN_MODULE(VideoEditorModule, NSObject)
 
-RCT_EXTERN_METHOD(openVideoEditor: (RCTPromiseResolveBlock)resolve)
+RCT_EXTERN_METHOD(openVideoEditor: (RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 
 @end
