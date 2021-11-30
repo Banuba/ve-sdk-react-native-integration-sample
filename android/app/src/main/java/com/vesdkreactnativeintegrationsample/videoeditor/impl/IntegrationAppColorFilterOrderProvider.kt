f2239a73f5502ea389855a5767e78c33eded2668
Alexander <alexander.bernat@banuba.com>
2021-11-30 17:12:41 +0300

    migrate android to 1.20, update resources

---
+++ android/app/src/main/java/com/vesdkreactnativeintegrationsample/videoeditor/impl/IntegrationAppColorFilterOrderProvider.kt
@@ -0,0 +1,34 @@
+package com.vesdkreactnativeintegrationsample.videoeditor.impl
+
+import com.banuba.sdk.core.data.OrderProvider
+
+class IntegrationAppColorFilterOrderProvider : OrderProvider {
+
+    override fun provide() = listOf(
+        "egypt",
+        "byers",
+        "chile",
+        "hyla",
+        "new_zeland",
+        "korben",
+        "canada",
+        "remy",
+        "england",
+        "retro",
+        "norway",
+        "neon",
+        "japan",
+        "instant",
+        "lux",
+        "sunset",
+        "bubblegum",
+        "chroma",
+        "lilac",
+        "pinkvine",
+        "spark",
+        "sunny",
+        "vinyl",
+        "glitch",
+        "grunge"
+    )
+}
