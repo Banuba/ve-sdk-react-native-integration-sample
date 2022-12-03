0cf1378d2260bab99205982bfee1df9c9170c358
Gleb Prischepa <gleb.prischepa@banuba.com>
2022-12-03 18:06:49 +0300

    Migrate to AppDelegate swift

--- ios/VideoEditorModuleBridge.m
+++ ios/VideoEditorModuleBridge.m
@@ -1,5 +1,11 @@
 
 #import <React/RCTBridgeModule.h>
+#import <React/RCTBridge.h>
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTRootView.h>
+#import <React/RCTUtils.h>
+#import <React/RCTConvert.h>
+#import <React/RCTBundleURLProvider.h>
 
 @interface RCT_EXTERN_MODULE(VideoEditorModule, NSObject)
 
