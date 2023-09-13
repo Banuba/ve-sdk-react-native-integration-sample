5982246eac006850fc92e333683349bbca0d9106
GlebPBanuba <67113090+GlebPBanuba@users.noreply.github.com>
2023-09-13 20:42:51 +0300

    Support React 0.72.4

--- ios/vesdkreactnativeintegrationsample-Bridging-Header.h
+++ ios/vesdkreactnativeintegrationsample-Bridging-Header.h
@@ -2,4 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <React/RCTBridgeModule.h>>
+#import <RCTAppDelegate.h>
+#import <React/RCTBundleURLProvider.h>
+#import <Expo/Expo.h>
